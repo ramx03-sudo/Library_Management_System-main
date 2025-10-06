169a70374b5fa2b40d2f3386a22b97f125ef4afc
ramx03-sudo <bhaskarsriram4321@gmail.com>
2025-10-06 09:44:21 +0530

    Your commit message here

--- Backend/src/main/java/com/lms/LibraryManagementSystem/LibraryManagementSystemApplication.java
+++ Backend/src/main/java/com/lms/LibraryManagementSystem/LibraryManagementSystemApplication.java
@@ -2,12 +2,20 @@ package com.lms.LibraryManagementSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LibraryManagementSystemApplication {
+public class LibraryManagementSystemApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LibraryManagementSystemApplication.class, args);
-	}
+    // 👇 This method allows your app to be deployed to an external Tomcat container
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(LibraryManagementSystemApplication.class);
+    }
 
+    // 👇 This main method still allows you to run it as a normal Spring Boot JAR
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManagementSystemApplication.class, args);
+    }
 }
